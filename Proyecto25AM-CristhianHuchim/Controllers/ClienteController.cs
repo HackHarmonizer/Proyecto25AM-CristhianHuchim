9f7ffd515b96378f3dc737130307458beec305ba
Cristhian <crishuc963@outlook.com>
2023-10-09 02:24:34 -0500

    Firts Contribution

--- Proyecto25AM-CristhianHuchim/Controllers/ClienteController.cs
+++ Proyecto25AM-CristhianHuchim/Controllers/ClienteController.cs
@@ -10,7 +10,39 @@ namespace Proyecto25AM_CristhianHuchim.Controllers
     {
         private readonly IClienteServices _ClienteServices;
 
-      
+        public ClienteController(IClienteServices usuarioServices)
+        {
+            _ClienteServices = usuarioServices;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MostrarC()
+        {
+            return Ok(await _ClienteServices.MostratCliente());
+        }
+        [HttpPost]
+        [Route("crearcli")]
+        public async Task<IActionResult> CrearC([FromBody] ClienteResponse request)
+        {
+            return Ok(await _ClienteServices.CrearCliente(request));
+        }
+        [HttpPut("actualizarcliente/{id}")]
+        public async Task<IActionResult> ActualizarC([FromBody] ClienteResponse request, int id)
+        {
+            return Ok(await _ClienteServices.ActualizarCliente(request, id));
+        }
+        [HttpDelete("eliminarcliente/{id:int}")]
+        public async Task<IActionResult> EliminarClie(int id)
+        {
+            return Ok(await _ClienteServices.EliminarCliente(id));
+        }
+
+        [HttpGet("consultarid/{id:int}")]
+        public async Task<IActionResult> MostrarC(int id)
+        {
+            return Ok(await _ClienteServices.MosID(id));
+        }
+
 
     }
 }
